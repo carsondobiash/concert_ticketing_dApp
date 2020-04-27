1f4c72b1929994cf886e3b4b5ace55d86e721913
suyashhiray <46005766+suyashhiray@users.noreply.github.com>
2020-04-27 03:56:27 -0700

    Update ticketownership.sol

--- contracts/ticketownership.sol
+++ contracts/ticketownership.sol
@@ -7,6 +7,9 @@ import "./ticketfactory.sol";
 /// TODO: Replace this with natspec descriptions
 contract TicketOwnership is TicketFactory, ERC721 {
 
+    address public buyer;
+    uint256 public number_tickets;
+
     using SafeMath for uint256;
 
     constructor (uint _sellingTime, address _owner, uint _price, uint _availableTickets) public {
@@ -23,6 +26,42 @@ contract TicketOwnership is TicketFactory, ERC721 {
     modifier onlyOwnerOf(uint _ticketId) {
         require(msg.sender == ticketToOwner[_ticketId]);
         _;
+    }
+
+    modifier an_ongoing_sale(){
+        require(now <= sale_end);
+        _;
+    }
+
+    modifier only_buyer(){
+        require(msg.sender==buyer);
+        _;
+    }
+
+    function buy_resale() public payable an_ongoing_sale returns (bool){
+        buyer = msg.sender;
+        number_tickets = msg.value;
+
+        return true;
+    }
+
+
+    function cancel_resale() external onlyOwnerOf an_ongoing_sale returns (bool){
+        STATE=sale_state.CANCELLED;
+        sale_end = now;
+        return true;
+    }
+
+    function resell() external only_buyer payable{
+        require(now < sale_end || STATE != sale_state.CANCELLED);
+        address payable _sale_owner = address(uint160(venue_owner));
+
+        _sale_owner.transfer(price * number_tickets);
+
+        address payable current = address(uint160(msg.sender));
+        //Need help here figuring out the transfer of the actual tickets
+        
+
     }
 
     function balanceOf(address _owner) external view returns (uint256) {
